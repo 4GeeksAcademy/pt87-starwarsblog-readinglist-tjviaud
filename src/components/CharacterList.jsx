75c88f5229f74bb9bc8ad1139ad85ce7ce85cc06
TJ Viaud <tjviaud@gmail.com>
2026-01-09 00:20:50 +0000

    saving

---
+++ src/components/CharacterList.jsx
@@ -0,0 +1,30 @@
+<div className="CharacterList">
+
+    <h1>Characters</h1>
+    
+<div class="card" style="width: 18rem;">
+  <img src="https://lumiere-a.akamaihd.net/v1/images/628cdaa1dbbde50001de0bd3-image_6c311046.jpeg?region=192%2C0%2C1152%2C864" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Obi-Wan Kenobi</h5>
+    <p class="card-text">Gender: Male Hair color: Eye color:</p>
+    <a href="#" class="btn btn-primary">Learn more!</a>
+  </div>
+  
+  <div class="card" style="width: 18rem;">
+  <img src="https://saberspro.com/cdn/shop/articles/anakin.jpg?crop=center&height=1200&v=1750086089&width=1200" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Anakin Skywalker</h5>
+    <p class="card-text">Gender: Male Hair color: Eye color:</p>
+    <a href="#" class="btn btn-primary">Learn more!</a>
+  </div>
+
+  <div class="card" style="width: 18rem;">
+  <img src="https://www.denofgeek.com/wp-content/uploads/2020/11/the-mandalorian-grogu-baby-yoda-name-origin.jpg?resize=768%2C432" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Grogu</h5>
+    <p class="card-text">Gender: Male Hair color: Eye color:</p>
+    <a href="#" class="btn btn-primary">Learn more!</a>
+  </div>
+</div>
+</div>
+</div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ export const Home = () => {
 
   const {store, dispatch} =useGlobalReducer()
 
+  
+
 	return (
 		<div className="text-center mt-5">
 			<h1>Hello Rigo!!</h1>
